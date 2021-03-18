d26806e32db323df17942c035c97387f32cc822d
Prakash07-er <71871887+Prakash07-er@users.noreply.github.com>
2021-03-18 11:42:10 +0530

    Add files via upload

---
+++ Rotate the elements by k times.js
@@ -0,0 +1,26 @@
+
+let rotate = function (n)
+
+{
+ 
+let a = [1,2,3,4,5];
+ 
+let rotate= n
+
+for (i=0 ; i<rotate ; i++)
+
+{
+    
+ temp = a.shift()
+
+ 
+   a.push(temp)
+
+
+}
+
+return a
+
+}
+
+console.log(rotate(2))
